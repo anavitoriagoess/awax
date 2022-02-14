d001cc945485e72f885ea8a2e9e9aaed4b24ebc4
Ana Vitoria <anavitoria.goes@hotmail.com>
2022-02-14 14:32:30 -0300

    projeto awax

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+function menuTogger() {
+    let menuArea = document.getElementById(nav)
+
+    if(menuArea.style.display == 'none') {
+        menuArea.style.display = 'flex'
+    } else{
+        menuArea.style.display = 'none'
+    }
+
+
+}
+
